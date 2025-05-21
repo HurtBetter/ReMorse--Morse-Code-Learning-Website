bde7a9b380e8c70b9bcf3912918409b092e650be
HurtBetter <ethanyhua@gmail.com>
2025-05-21 00:21:32 -0400

    Create Home.jsx

---
+++ Home.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+function Home() {
+  return (
+    <div className="Home">
+      <div className="about">
+        <h1 className="title">What is ReMorse?</h1>
+        <p class="blurb">ReMorse is a learning toolset for radioheads and Morse code enthusiasts. Whether you're starting out or already going, we're here to help you along.</p>
+      </div>
+
+      <div className="impetus">
+        <h1 className="title">Why should you use ReMorse</h1>
+        <p class="blurb">We have your family.</p>
+        <p class="blurb">Use the app.</p>
+        <p class="blurb">You'll receive a message when you reach the threshold of understanding.</p>
+        <p class="blurb">We are watching.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
